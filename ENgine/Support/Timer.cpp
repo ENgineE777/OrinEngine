9c904de3022cc862b03281f74114940c8a1c1c55
e.solyanov <e.solyanov>
2020-08-31 00:22:50 -0700

    small cleanup

--- ENgine/Support/Timer.cpp
+++ ENgine/Support/Timer.cpp
@@ -1,6 +1,12 @@
 #include "Timer.h"
 #include <stdio.h>
 
+#ifdef PLATFORM_WIN
+#define WIN32_LEAN_AND_MEAN
+#include <time.h>
+#include <Windows.h>
+#endif
+
 namespace Oak::Timer
 {
 	#ifdef PLATFORM_WIN
--- ENgine/Support/Timer.h
+++ ENgine/Support/Timer.h
@@ -1,11 +1,5 @@
 #pragma once
 
-#ifdef PLATFORM_WIN
-#define WIN32_LEAN_AND_MEAN
-#include <time.h>
-#include <Windows.h>
-#endif
-
 namespace Oak::Timer
 {
 	float CountDeltaTime();
