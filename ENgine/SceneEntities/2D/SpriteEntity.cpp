23459b8ef132dc4853a1e6315e0bb98f69f24e61
e.solyanov <e.solyanov>
2021-05-19 23:14:08 -0700

    Missing files

---
+++ ENgine/SceneEntities/2D/SpriteEntity.cpp
@@ -0,0 +1,59 @@
+
+#include "SpriteEntity.h"
+#include "Root/Root.h"
+
+#include "Support/MetaData.h"
+
+
+namespace Oak
+{
+	CLASSREG(SceneEntity, SpriteEntity, "Sprite")
+
+	META_DATA_DESC(SpriteEntity)
+		BASE_SCENE_ENTITY_PROP(SpriteEntity)
+		FLOAT_PROP(SpriteEntity, transform.local.Pos().x, 0.0f, "Transform", "x", "X coordinate of a sprite")
+		FLOAT_PROP(SpriteEntity, transform.local.Pos().y, 0.0f, "Transform", "y", "Y coordinate of a sprite")
+		FLOAT_PROP(SpriteEntity, transform.local.Pos().z, 0.0f, "Transform", "depth", "Depth from a camera position")
+		FLOAT_PROP(SpriteEntity, transform.rotation.z, 0.0f, "Transform", "rotation", "Rotation of a sprite")
+		FLOAT_PROP(SpriteEntity, transform.size.x, 100.0f, "Transform", "width", "Width of a sprite")
+		FLOAT_PROP(SpriteEntity, transform.size.y, 100.0f, "Transform", "height", "Height of a sprite")
+		FLOAT_PROP(SpriteEntity, transform.offset.x, 0.5f, "Transform", "offset X", "X coordinate of anchorn in absolute units")
+		FLOAT_PROP(SpriteEntity, transform.offset.y, 0.5f, "Transform", "offset Y", "Y coordinate of anchorn in absolute units")
+		ASSET_TEXTURE_PROP(SpriteEntity, texture, "Visual", "Texture")
+	META_DATA_DESC_END()
+
+	Sprite::FrameState SpriteEntity::frameState;
+
+	SpriteEntity::SpriteEntity() : SceneEntity()
+	{
+		//inst_class_name = "SpriteInst";
+	}
+
+	void SpriteEntity::Init()
+	{
+		sprite.frames.push_back(Sprite::Frame());
+
+		RenderTasks(false)->AddTask(0, this, (Object::Delegate)& SpriteEntity::Draw);
+
+		GetScene()->AddToGroup(this, "SpriteAsset");
+	}
+
+	void SpriteEntity::ApplyProperties()
+	{
+		sprite.texture = texture ? texture->GetTexture() : root.render.GetWhiteTexture();
+		transform.size = Math::Vector3((float)sprite.texture->GetWidth(), (float)sprite.texture->GetHeight(), 0.0f);
+	}
+
+	Transform* SpriteEntity::GetTransform()
+	{
+		return &transform;
+	}
+
+	void SpriteEntity::Draw(float dt)
+	{
+		transform.BuildMatrices();
+		Sprite::UpdateFrame(&sprite, &frameState, dt);
+
+		Sprite::Draw(&transform, COLOR_WHITE, &sprite, &frameState, true, false);
+	}
+}
---
+++ ENgine/SceneEntities/2D/SpriteEntity.h
@@ -0,0 +1,35 @@
+
+#pragma once
+
+#include "Root/Scenes/SceneEntity.h"
+#include "Support/MetaData.h"
+#include "Support/Sprite.h"
+#include "root/Assets/AssetTexture.h"
+
+namespace Oak
+{
+	class SpriteEntity : public SceneEntity
+	{
+	public:
+
+		Transform transform;
+
+		Sprite::Data sprite;
+
+		AssetTextureRef texture;
+
+		META_DATA_DECL_BASE(SpriteEntity)
+
+	#ifndef DOXYGEN_SKIP
+		static Sprite::FrameState frameState;
+
+		SpriteEntity();
+		virtual ~SpriteEntity() = default;
+
+		void Init() override;
+		void ApplyProperties() override;
+		void Draw(float dt);
+		Transform* GetTransform() override;
+	#endif
+	};
+}
