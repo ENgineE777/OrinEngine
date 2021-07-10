66377d8d2903d5ab7aa92de3bc8160387a534d18
e.solyanov <e.solyanov>
2021-07-10 11:28:51 -0700

    More missing files

---
+++ ENgine/SceneEntities/2D/ParalaxedSpriteEntity.cpp
@@ -0,0 +1,40 @@
+
+#include "SpriteEntity.h"
+#include "Root/Root.h"
+
+namespace Oak
+{
+	/*CLASSREG(SceneEntity, SpriteEntity, "Sprite")
+
+	META_DATA_DESC(SpriteEntity)
+		BASE_SCENE_ENTITY_PROP(SpriteEntity)
+		ASSET_TEXTURE_PROP(SpriteEntity, texture, "Visual", "Texture")
+	META_DATA_DESC_END()
+
+	SpriteEntity::SpriteEntity() : SceneEntity()
+	{
+		//inst_class_name = "SpriteInst";
+	}
+
+	void SpriteEntity::Init()
+	{
+		transform.unitsScale = &Sprite::pixelsPerUnit;
+		transform.unitsInvScale = &Sprite::pixelsPerUnitInvert;
+		transform.transformFlag = SpriteTransformFlags;
+
+		Tasks(true)->AddTask(0, this, (Object::Delegate)&SpriteEntity::Draw);
+	}
+
+	void SpriteEntity::ApplyProperties()
+	{
+		Math::Vector2 size = texture.GetSize();
+		transform.size = Math::Vector3(size.x, size.y, 0.0f);
+	}
+
+	void SpriteEntity::Draw(float dt)
+	{
+		transform.BuildMatrices();
+
+		texture.Draw(&transform, COLOR_WHITE, dt);
+	}*/
+}
---
+++ ENgine/SceneEntities/2D/ParalaxedSpriteEntity.h
@@ -0,0 +1,29 @@
+
+#pragma once
+
+#include "Root/Scenes/SceneEntity.h"
+#include "Support/MetaData.h"
+#include "Support/Sprite.h"
+#include "root/Assets/AssetTexture.h"
+
+namespace Oak
+{
+	/*class SpriteEntity : public SceneEntity
+	{
+	public:
+
+		AssetTextureRef texture;
+
+		META_DATA_DECL_BASE(SpriteEntity)
+
+	#ifndef DOXYGEN_SKIP
+
+		SpriteEntity();
+		virtual ~SpriteEntity() = default;
+
+		void Init() override;
+		void ApplyProperties() override;
+		void Draw(float dt);
+	#endif
+	};*/
+}
