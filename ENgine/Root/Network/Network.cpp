19cb91c546652c0ad563499bed02f52a0f667b6e
e.solyanov <e.solyanov>
2021-07-13 21:45:44 -0700

    Adding Network helpers

---
+++ ENgine/Root/Network/Network.cpp
@@ -0,0 +1,306 @@
+#include "Network.h"
+#include "Root/Root.h"
+
+
+#ifdef PLATFORM_WIN
+#pragma comment (lib, "Ws2_32.lib")
+#endif
+
+namespace Oak
+{
+	NetworkClient::NetworkClient()
+	{
+		send_buffer = (char*)malloc(BUFFSIZE);
+		recv_buffer = (char*)malloc(BUFFSIZE);
+	}
+
+	NetworkClient::~NetworkClient()
+	{
+		free(send_buffer);
+		free(recv_buffer);
+	}
+
+	bool NetworkClient::Connect(const char* ip, int port)
+	{
+	#ifdef PLATFORM_WIN
+		struct addrinfo *result = nullptr, *ptr = nullptr;
+		struct addrinfo hints;
+
+		WSADATA wsaData;
+
+		int res = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+		if (res != 0)
+		{
+			root.Log("Network","WSAStartup failed with error: %d", res);
+			return false;
+		}
+
+		ZeroMemory(&hints, sizeof(hints));
+		hints.ai_family = AF_UNSPEC;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_protocol = IPPROTO_TCP;
+
+		char strPort[128];
+		StringUtils::Printf(strPort, 128, "%i", port);
+
+		res = getaddrinfo(ip, strPort, &hints, &result);
+
+		if (res != 0)
+		{
+			root.Log("Network", "getaddrinfo failed with error: %d", res);
+			return false;
+		}
+
+		for (ptr = result; ptr != NULL; ptr = ptr->ai_next)
+		{
+			tcpSocket = socket(ptr->ai_family, ptr->ai_socktype, ptr->ai_protocol);
+
+			if (tcpSocket == INVALID_SOCKET)
+			{
+				root.Log("Network", "socket failed with error: %ld", WSAGetLastError());
+				return false;
+			}
+
+			res = connect(tcpSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+
+			if (res == SOCKET_ERROR)
+			{
+				closesocket(tcpSocket);
+				tcpSocket = INVALID_SOCKET;
+				root.Log("Network", "The server is down... did not connect");
+			}
+		}
+
+		freeaddrinfo(result);
+
+		if (tcpSocket == INVALID_SOCKET)
+		{
+			root.Log("Network", "Unable to connect to server!");
+			return false;
+		}
+		u_long iMode = 1;
+
+		res = ioctlsocket(tcpSocket, FIONBIO, &iMode);
+	
+		if (res == SOCKET_ERROR)
+		{
+			root.Log("Network", "ioctlsocket failed with error: %d", WSAGetLastError());
+			closesocket(tcpSocket);
+			return false;
+		}
+
+		char value = 1;
+		setsockopt(tcpSocket, IPPROTO_TCP, TCP_NODELAY, &value, sizeof(value));
+	#endif
+
+		return true;
+	}
+
+	#ifdef PLATFORM_WIN
+	void NetworkClient::SetSocket(int id, SOCKET socket)
+	{
+		this->tcpSocket = socket;
+		this->id = id;
+	}
+	#endif
+
+	void NetworkClient::Send(void* data, int len)
+	{
+		CheckCapacity(send_buffer, send_buff_size, send_len + len);
+
+		memcpy(&send_buffer[send_len], data, len);
+		send_len += len;
+	}
+
+	void NetworkClient::Recive()
+	{
+	#ifdef PLATFORM_WIN
+		char chunk[CHUNKSIZE];
+
+		char* buffer = recv_buffer;
+
+		while (true)
+		{
+			int rec = recv(tcpSocket, chunk, CHUNKSIZE, 0);
+
+			if (rec == -1)
+			{
+				break;
+			}
+
+			if (rec == 0)
+			{
+				root.Log("Network", "Connection closed");
+				closesocket(tcpSocket);
+				break;
+			}
+
+			recv_len += rec;
+
+			CheckCapacity(recv_buffer, recv_buff_size, recv_len);
+
+			memcpy(buffer, chunk, rec);
+			buffer += rec;
+
+			if (rec < CHUNKSIZE)
+			{
+				break;
+			}
+		}
+	#endif
+	}
+
+	void NetworkClient::Update()
+	{
+		Recive();
+
+		if (recv_len)
+		{
+			if (delegedate)
+			{
+				delegedate->OnDataRecieved(id, recv_buffer, recv_len);
+			}
+
+			recv_len = 0;
+		}
+
+	#ifdef PLATFORM_WIN
+		if (send_len)
+		{
+			send(tcpSocket, send_buffer, send_len, 0);
+			send_len = 0;
+		}
+	#endif
+	}
+
+	bool NetworkServer::Start(int port)
+	{
+	#ifdef PLATFORM_WIN
+		WSADATA wsaData;
+
+		int res = WSAStartup(MAKEWORD(2, 2), &wsaData);
+
+		if (res != 0)
+		{
+			root.Log("Network", "WSAStartup failed with error: %d", res);
+			return false;
+		}
+
+		struct addrinfo *result = nullptr;
+		struct addrinfo hints;
+
+		ZeroMemory(&hints, sizeof(hints));
+		hints.ai_family = AF_INET;
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_protocol = IPPROTO_TCP;
+		hints.ai_flags = AI_PASSIVE;
+
+		char strPort[32];
+		StringUtils::Printf(strPort, 32, "%i", port);
+
+		res = getaddrinfo(NULL, strPort, &hints, &result);
+
+		if (res != 0)
+		{
+			root.Log("Network", "getaddrinfo failed with error: %d", res);
+			return false;
+		}
+
+		listenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+
+		if (listenSocket == INVALID_SOCKET)
+		{
+			root.Log("Network", "socket failed with error: %ld", WSAGetLastError());
+			freeaddrinfo(result);
+			return false;
+		}
+
+		u_long iMode = 1;
+		res = ioctlsocket(listenSocket, FIONBIO, &iMode);
+
+		if (res == SOCKET_ERROR)
+		{
+			root.Log("Network", "ioctlsocket failed with error: %d", WSAGetLastError());
+			closesocket(listenSocket);
+			return false;
+		}
+
+		res = bind(listenSocket, result->ai_addr, (int)result->ai_addrlen);
+
+		if (res == SOCKET_ERROR)
+		{
+			int k = WSAGetLastError();
+			root.Log("Network", "bind failed with error: %d", WSAGetLastError());
+			freeaddrinfo(result);
+			closesocket(listenSocket);
+			return false;
+		}
+
+		freeaddrinfo(result);
+
+		res = listen(listenSocket, SOMAXCONN);
+
+		if (res == SOCKET_ERROR)
+		{
+			root.Log("Network", "listen failed with error: %d", WSAGetLastError());
+			closesocket(listenSocket);
+			return false;
+		}
+	#endif
+
+		return true;
+	}
+
+	void NetworkServer::Update()
+	{
+	#ifdef PLATFORM_WIN
+		SOCKET clientSocket = accept(listenSocket, NULL, NULL);
+
+		if (clientSocket != INVALID_SOCKET)
+		{
+			char value = 1;
+			setsockopt(clientSocket, IPPROTO_TCP, TCP_NODELAY, &value, sizeof(value));
+
+			client_id++;
+
+			NetworkClient* client = new NetworkClient();
+			client->SetSocket(client_id, clientSocket);
+
+			client->delegedate = delegedate;
+
+			clients.push_back(client);
+
+			if (delegedate)
+			{
+				delegedate->OnClientConnected(client_id);
+			}
+		}
+	#endif
+
+		for (auto client : clients)
+		{
+			client->Update();
+		}
+	}
+
+	void NetworkServer::Send2Client(int id, void* data, int len)
+	{
+		for (auto client : clients)
+		{
+			if (client->id == id)
+			{
+				client->Send(data, len);
+				break;
+			}
+		}
+	}
+
+	void NetworkServer::Send2All(void* data, int len)
+	{
+		for (auto client : clients)
+		{
+			client->Send(data, len);
+		}
+	}
+}
---
+++ ENgine/Root/Network/Network.h
@@ -0,0 +1,178 @@
+#pragma once
+
+#ifdef PLATFORM_WIN
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#endif
+#include <stdio.h> 
+#include <stdlib.h> 
+#include "Support/Support.h" 
+
+namespace Oak
+{
+	/**
+		\ingroup gr_code_services_network
+	*/
+
+	/**
+		\brief NetworkDelegate
+
+		Delegate that handles network callbacks.
+	*/
+
+	class CLASS_DECLSPEC NetworkDelegate
+	{
+		public:
+
+		/**
+			\brief Callback on reciving data 
+
+			\param[out] client_id ID of a clinet
+			\param[out] data Pointer to a recived data
+			\param[out] size Size in bytes of a recived data
+		*/
+		virtual void OnDataRecieved(int client_id, void* data, int size) {};
+
+		/**
+			\brief OnClientConnected
+
+			\param id Id of a newly connected client
+		*/
+		virtual void OnClientConnected(int id) {};
+	};
+
+	/**
+	\ingroup gr_code_services_network
+	*/
+
+	/**
+		\brief NetworkClient
+
+		Client that conects to a server via TCP connection. IP address and a port of a server is needed
+		for a connection. For handling callbacks from network client a delegate should be set.
+	*/
+
+	class CLASS_DECLSPEC NetworkClient
+	{ 
+	#ifndef DOXYGEN_SKIP
+		enum
+		{
+			CHUNKSIZE = 512,
+			BUFFSIZE = 16384
+		};
+
+	#ifdef PLATFORM_WIN
+		SOCKET tcpSocket = INVALID_SOCKET;
+	#endif
+		char* send_buffer = nullptr;
+		int  send_len = 0;
+		int  send_buff_size = BUFFSIZE;
+
+		char* recv_buffer = nullptr;
+		int  recv_len = 0;
+		int  recv_buff_size = BUFFSIZE;
+	#endif
+	public:
+
+	#ifndef DOXYGEN_SKIP
+		inline void CheckCapacity(char* &buff, int &buff_size, int needed_size)
+		{
+			if (needed_size > send_buff_size)
+			{
+				while (needed_size > send_buff_size)
+				{
+					send_buff_size += BUFFSIZE;
+				}
+
+				realloc(send_buffer, send_buff_size);
+			}
+		}
+
+		NetworkDelegate* delegedate = nullptr;
+
+		NetworkClient();
+		~NetworkClient();
+
+		int id = -1;
+	#endif
+
+		/**
+			\brief Connect to a server
+
+			\param[in] ip Address of a server
+			\param[in] port Port of a server
+
+			\return True will be returned on successfull conection to a server. Otherwise false will be returned.
+		*/
+		bool Connect(const char* ip, int port);
+		#ifdef PLATFORM_WIN
+		void SetSocket(int id, SOCKET tcpSocket);
+		#endif
+
+		/**
+			\brief Send data via socket
+
+			\param[in] data Pointer to a data
+			\param[in] len Size in bytes of a data
+		*/
+		void Send(void* data, int len);
+
+		#ifndef DOXYGEN_SKIP
+		void Recive();
+		void Update();
+	#endif
+	};
+
+	/**
+		\ingroup gr_code_services_network
+	*/
+
+	/**
+		\brief NetworkServer
+
+		Server which can accept connection of clients via TCP connection.
+		For handling callbacks from network client a delegate should be set.
+	*/
+
+	class CLASS_DECLSPEC NetworkServer
+	{
+	#ifndef DOXYGEN_SKIP
+		#ifdef PLATFORM_WIN
+		SOCKET listenSocket = INVALID_SOCKET;
+		#endif
+		eastl::vector<NetworkClient*> clients;
+		int client_id = 0;
+	#endif
+
+	public:
+
+	#ifndef DOXYGEN_SKIP
+		NetworkDelegate* delegedate = nullptr;
+		void Update();
+	#endif
+
+		/**
+			\brief Start a server
+
+			\param port Port of binding of a server
+		*/
+		bool Start(int port);
+
+		/**
+			\brief Send data to a particular client
+
+			\param[in] id Id of a client
+			\param[in] data Pointer to a sending data
+			\param[in] len Size in bytes of a senfing data
+		*/
+		void Send2Client(int id, void* data, int len);
+
+		/**
+			\brief Send data to a all connected clients
+
+			\param[in] data Pointer to a sending data
+			\param[in] len Size in bytes of a senfing data
+		*/
+		void Send2All(void* data, int len);
+	};
+}
