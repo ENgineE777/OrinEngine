ad4f9a38d7d675f1486b8af5b764e6ed8016b2cb
e.solyanov <e.solyanov>
2021-08-04 16:45:21 -0700

    Node2D

---
+++ ENgine/SceneEntities/2D/Node2D.cpp
@@ -0,0 +1,24 @@
+
+#include "Node2D.h"
+#include "Root/Root.h"
+
+namespace Oak
+{
+	CLASSREG(SceneEntity, Node2D, "Node2D")
+
+	META_DATA_DESC(Node2D)
+		BASE_SCENE_ENTITY_PROP(Node2D)
+	META_DATA_DESC_END()
+
+	Node2D::Node2D() : SceneEntity()
+	{
+		//inst_class_name = "SpriteInst";
+	}
+
+	void Node2D::Init()
+	{
+		transform.unitsScale = &Sprite::pixelsPerUnit;
+		transform.unitsInvScale = &Sprite::pixelsPerUnitInvert;
+		transform.transformFlag = TransformFlag::MoveXYZ | TransformFlag::RotateZ | TransformFlag::ScaleX | TransformFlag::ScaleY;
+	}
+}
---
+++ ENgine/SceneEntities/2D/Node2D.h
@@ -0,0 +1,25 @@
+
+#pragma once
+
+#include "Root/Scenes/SceneEntity.h"
+#include "Support/MetaData.h"
+#include "Support/Sprite.h"
+#include "root/Assets/AssetTexture.h"
+
+namespace Oak
+{
+	class Node2D : public SceneEntity
+	{
+	public:
+
+		META_DATA_DECL_BASE(Node2D)
+
+	#ifndef DOXYGEN_SKIP
+
+		Node2D();
+		virtual ~Node2D() = default;
+
+		void Init() override;
+	#endif
+	};
+}
