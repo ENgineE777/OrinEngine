be74ccea82aa629fe91379be8f8543f7eb24c923
ENgineE777 <enginee777@gmail.com>
2021-08-23 00:14:28 -0700

    batch file for copy headers

---
+++ Bin/CopyCppBuild.bat
@@ -0,0 +1,8 @@
+xcopy ..\Libs\vjson\*.h ENgine\CppBuild\include\Libs\vjson\ /sy
+xcopy ..\Libs\eastl\include\*.* ENgine\CppBuild\include\Libs\eastl\include\ /sy
+xcopy ..\Libs\fmod\include\*.* ENgine\CppBuild\include\Libs\fmod\include\ /sy
+xcopy ..\Libs\physx\include\*.* ENgine\CppBuild\include\Libs\physx\include\ /sy
+xcopy ..\ENgine\*.h ENgine\CppBuild\include\ENgine\ /sy
+copy Oak.dll ENgine\ExportBinaries\Oak.dll
+
+pause
