2d59eeb2096b00392d4e4577195cc364ea8bd62d
ENgineE777 <enginee777@gmail.com>
2021-12-07 23:04:16 -0800

    Viewport in tileSet

--- ENgine/Editor/TileSetWindow.cpp
+++ ENgine/Editor/TileSetWindow.cpp
@@ -46,7 +46,7 @@ namespace Oak
 	{
 		if (!opened)
 		{
-			//texture = nullptr;
+			tileSet = nullptr;
 			return;
 		}
 
@@ -64,7 +64,7 @@ namespace Oak
 			needSetSize = false;
 		}
 
-		/*ImGuiID dockspaceID = ImGui::GetID("SpriteEditor");
+		ImGuiID dockspaceID = ImGui::GetID("SpriteEditor");
 
 		ImGuiDockNodeFlags dockspaceFlags = ImGuiDockNodeFlags_None;
 
@@ -74,115 +74,70 @@ namespace Oak
 			ImGui::DockBuilderAddNode(dockspaceID, ImGuiDockNodeFlags_None);
 
 			ImGuiID dock_main_id = dockspaceID;
-			ImGuiID dock_top_id = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Up, 0.6f, nullptr, &dock_main_id);
-			ImGuiID dock_bottom_id = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Down, 0.25f, nullptr, &dock_main_id);
+			ImGuiID dock_top_id = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Up, 0.1f, nullptr, &dock_main_id);
+			ImGuiID dock_bottom_id = ImGui::DockBuilderSplitNode(dock_main_id, ImGuiDir_Down, 0.9f, nullptr, &dock_main_id);
 
-			ImGui::DockBuilderDockWindow("Image Info", dock_bottom_id);
-			ImGui::DockBuilderDockWindow("Auto slicing", dock_bottom_id);
-			ImGui::DockBuilderDockWindow("Selected Slices", dock_bottom_id);
-			ImGui::DockBuilderDockWindow("Animations", dock_bottom_id);
-			ImGui::DockBuilderDockWindow("Image", dock_top_id);
+			ImGui::DockBuilderDockWindow("ToolbarTileSet", dock_top_id);
+			ImGui::DockBuilderDockWindow("ImageTileSet", dock_bottom_id);
 
 			ImGui::DockBuilderFinish(dock_main_id);
 
 			ImGuiDockNode* node = ImGui::DockBuilderGetNode(dock_top_id);
 			node->LocalFlags |= ImGuiDockNodeFlags_NoTabBar;
+
+			node = ImGui::DockBuilderGetNode(dock_bottom_id);
+			node->LocalFlags |= ImGuiDockNodeFlags_NoTabBar;
 		}
 
-		ImGui::DockSpace(dockspaceID, ImVec2(0.0f, 0.0f), dockspaceFlags | ImGuiDockNodeFlags_NoCloseButton | ImGuiDockNodeFlags_NoWindowMenuButton);*/
+		ImGui::DockSpace(dockspaceID, ImVec2(0.0f, 0.0f), dockspaceFlags | ImGuiDockNodeFlags_NoCloseButton | ImGuiDockNodeFlags_NoWindowMenuButton);
 
 		ImGui::End();
 
-		/*ShowImageInfo();
-		ShowAutoSlicing();
-		ShowSlices();
-		ShowAnimations();
-		ShowImage();*/
+		ShowToolbar();
+		ShowImage();
 	}
 
 	void TileSetWindow::DrawViewport(Math::Vector2 viewportSize)
 	{
-		/*lastViewportSize = viewportSize;
+		lastViewportSize = viewportSize;
 
 		root.render.GetDevice()->SetBackBuffer(1, (int)viewportSize.x, (int)viewportSize.y, &editorDrawer.hwnd);
 
 		root.render.GetDevice()->Clear(true, COLOR_GRAY, true, 1.0f);
 
 		editorDrawer.SetCameraMatrices(camPos, viewportSize.y / camZoom, viewportSize.y / viewportSize.x);
-	
-		editorDrawer.DrawCheckerBoard(camPos, viewportSize, camZoom);
 
 		Color color = COLOR_WHITE;
 
-		Math::Matrix spriteTrans;
-		spriteTrans.Pos().y = texture->size.y;
-
-		Sprite::Draw(nullptr, COLOR_GRAY_A(0.65f), spriteTrans, 0.0f, texture->size, 0.0f, 1.0f, Sprite::quadPrgNoZ);
-		Sprite::Draw(texture->texture, COLOR_WHITE, spriteTrans, 0.0f, texture->size, 0.0f, 1.0f, Sprite::quadPrgNoZ);
-
-		for (int i = 0; i < texture->slices.size(); i++)
 		{
-			if (selSlice == i)
+			Math::Vector2 step = { (float)tileSet->sizeX, (float)tileSet->sizeY };
+
+			float minStep = 16.0f;
+
+			Math::Vector2 pos = camPos;
+
+			pos.x = step.x * (int)(pos.x / step.x);
+			pos.y = step.y * (int)(pos.y / step.y);
+
+			pos *= Sprite::pixelsPerUnitInvert;
+			step *= Sprite::pixelsPerUnitInvert;
+
+			Color color = COLOR_WHITE_A(0.75f);
+
+			int numCellsY = 30;
+			int numCellsX = 45;
+
+			numCellsX = (int)(viewportSize.y * 0.5f * Sprite::pixelsPerUnitInvert / root.render.GetDevice()->GetAspect() / camZoom / step.y + 2);
+			numCellsY = (int)(viewportSize.y * 0.5f * Sprite::pixelsPerUnitInvert / camZoom / step.y + 2);
+
+			for (int i = -numCellsY; i <= numCellsY; i++)
 			{
-				continue;
+				root.render.DebugLine(Math::Vector3(-numCellsX * step.x + pos.x, i * step.y + pos.y, 0.0f), color, Math::Vector3(numCellsX * step.x + pos.x, i * step.y + pos.y, 0.0f), color, false);
 			}
 
-			auto& slice = texture->slices[i];
-			Math::Vector2 pos(slice.pos.x, texture->size.y - slice.pos.y);
-			DrawRect(pos, pos + Math::Vector2(slice.size.x, -slice.size.y), color);
-		}
-
-		if (drag == Drag::DragNewSlice)
-		{
-			DrawRect(rectStart, rectEnd, color);
-		}
-
-		if (selSlice != -1)
-		{
-			for (int i = 0; i < rectHeight; i++)
-				for (int j = 0; j<rectWidth; j++)
-				{
-					int index = rectWidth * i + j;
-
-					color = COLOR_GREEN;
-
-					if (j < rectWidth - 1 && i < rectHeight - 1)
-					{
-						root.render.DebugLine((Math::Vector3(points[index + rectWidth].x, points[index + rectWidth].y, 0.0f)) * Sprite::pixelsPerUnitInvert, color,
-											  (Math::Vector3(points[index].x, points[index].y, 0.0f)) * Sprite::pixelsPerUnitInvert, color, false);
-
-						root.render.DebugLine((Math::Vector3(points[index].x, points[index].y, 0.0f)) * Sprite::pixelsPerUnitInvert, color,
-												(Math::Vector3(points[index + 1].x, points[index + 1].y, 0.0f)) * Sprite::pixelsPerUnitInvert, color, false);
-
-						if (i == rectHeight - 2 || j == rectWidth - 2)
-						{
-							root.render.DebugLine((Math::Vector3(points[index + 1].x, points[index + 1].y, 0.0f)) * Sprite::pixelsPerUnitInvert, color,
-													(Math::Vector3(points[index + rectWidth + 1].x, points[index + rectWidth + 1].y, 0.0f)) * Sprite::pixelsPerUnitInvert, color, false);
-
-							root.render.DebugLine((Math::Vector3(points[index + rectWidth + 1].x, points[index + rectWidth + 1].y, 0.0f)) * Sprite::pixelsPerUnitInvert, color,
-													(Math::Vector3(points[index + rectWidth].x, points[index + rectWidth].y, 0.0f)) * Sprite::pixelsPerUnitInvert, color, false);
-						}
-					}
-
-					color = COLOR_WHITE;
-
-					if (selRow == i && selCol == j)
-					{
-						color.Set(1.0, 0.9f, 0.0f, 1.0f);
-					}
-
-					Math::Vector3 pos = root.render.TransformToScreen((Math::Vector3(points[index].x, points[index].y, 0.0f)) * Sprite::pixelsPerUnitInvert, 2);
-					root.render.DebugSprite(editorDrawer.anchornTex, Math::Vector2(pos.x - 4, pos.y - 4), Math::Vector2(8.0f), color);
-				}
-		}
-		else
-		{
-			for (int i = 0; i < selectedSlices.size(); i++)
+			for (int i = -numCellsX; i <= numCellsX; i++)
 			{
-				auto& slice = texture->slices[selectedSlices[i]];
-
-				Math::Vector2 pos(slice.pos.x, texture->size.y - slice.pos.y);
-				DrawRect(pos, pos + Math::Vector2(slice.size.x, -slice.size.y), COLOR_GREEN);
+				root.render.DebugLine(Math::Vector3(i * step.x + pos.x, -numCellsY * step.y + pos.y, 0.0f), color, Math::Vector3(i * step.x + pos.x, numCellsY * step.y + pos.y, 0.0f), color, false);
 			}
 		}
 
@@ -193,202 +148,93 @@ namespace Oak
 
 		root.render.ExecutePool(1000, 0.0f);
 
-		root.render.GetDevice()->Present();*/
+		root.render.GetDevice()->Present();
+	}
+
+	void TileSetWindow::ShowToolbar()
+	{
+		ImGui::Begin("ToolbarTileSet");
+
+		ImGui::Dummy({20.0f, 20.0f});
+		ImGui::End();
+	}
+
+	void TileSetWindow::ShowImage()
+	{
+		ImGui::Begin("ImageTileSet");
+
+		ImGuiIO& io = ImGui::GetIO();
+
+		ImVec2 size = ImGui::GetContentRegionAvail();
+
+		ImVec2 viewportPos = ImVec2(io.MousePos.x - ImGui::GetCursorScreenPos().x, io.MousePos.y - ImGui::GetCursorScreenPos().y);
+
+		if (size.y < 0)	size.y = -size.y;
+
+		DrawViewport(Math::Vector2(size.x, size.y));
+
+		ImGui::Image(Oak::root.render.GetDevice()->GetBackBuffer(), size);
+
+		imageFocused = ImGui::IsWindowFocused();
+
+		if (imageFocused)
+		{
+			camZoom = Math::Clamp(camZoom + io.MouseWheel * 0.5f, 0.4f, 3.0f);
+		}
+
+		vireportHowered = ImGui::IsItemHovered();
+
+		if (vireportHowered && ImGui::IsMouseClicked(0))
+		{
+			OnLeftMouseDown();
+			viewportCaptured = true;
+		}
+		else
+		if (vireportHowered && ImGui::IsMouseClicked(2))
+		{
+			drag = Drag::DragField;
+			viewportCaptured = true;
+		}
+
+		ImVec2 del = ImGui::GetMouseDragDelta(0);
+		OnMouseMove(Math::Vector2((float)viewportPos.x, (float)viewportPos.y),
+					fabsf(del.x) > 7.0f && fabsf(del.y) > 7.0f && ImGui::IsWindowFocused() &&
+					viewportPos.x > 0 && viewportPos.x < size.x&& viewportPos.y > 0 && viewportPos.y < size.x);
+
+		if (viewportCaptured && ImGui::IsMouseReleased(0))
+		{
+			OnLeftMouseUp();
+			viewportCaptured = false;
+		}
+		else
+		if (viewportCaptured && ImGui::IsMouseReleased(2))
+		{
+			drag = Drag::DragNone;
+			viewportCaptured = false;
+		}
+
+		ImGui::End();
 	}
 
 	void TileSetWindow::OnMouseMove(Math::Vector2 ms, bool dragMouse)
 	{
-		/*Math::Vector2 delta(prevMs.x - ms.x, prevMs.y - ms.y);
+		Math::Vector2 delta(prevMs.x - ms.x, prevMs.y - ms.y);
 
 		if (drag == Drag::DragField)
 		{
 			camPos += Math::Vector2(delta.x, -delta.y) / camZoom;
-
-			camPos.x = Math::Clamp(camPos.x, 0.0f, texture->size.x);
-			camPos.y = Math::Clamp(camPos.y, 0.0f, texture->size.y);
 		}
-		else
-		if (drag == Drag::DragRects)
-		{
-			MoveRects(Math::Vector2(delta.x, delta.y) / camZoom);
-		}
-
+		
 		prevMs = ms;
-
-		if (!inDragAndDrop && dragMouse && drag == Drag::DragNone)
-		{
-			drag = Drag::DragNewSlice;
-		}
-
-		if (drag == Drag::DragNewSlice)
-		{
-			rectEnd = camPos + Math::Vector2(prevMs.x - lastViewportSize.x * 0.5f, -prevMs.y + lastViewportSize.y * 0.5f) / camZoom;
-		}*/
 	}
 
 	void TileSetWindow::OnLeftMouseDown()
 	{
-		/*if (inDragAndDrop || root.controls.DebugKeyPressed("KEY_LCONTROL", AliasAction::Pressed, true))
-		{
-			return;
-		}
-
-		Math::Vector2 ps(prevMs.x, -prevMs.y);
-
-		rectStart = camPos + Math::Vector2(prevMs.x - lastViewportSize.x * 0.5f, -prevMs.y + lastViewportSize.y * 0.5f) / camZoom;
-
-		int prevSelSlice = selSlice;
-		selSlice = -1;
-
-		bool multiSelected = root.controls.DebugKeyPressed("KEY_LSHIFT", AliasAction::Pressed, true);
-
-		float bufferZone = 3.0f;
-		for (int i = 0; i < texture->slices.size(); i++)
-		{
-			auto& slice = texture->slices[i];
-
-			if (slice.pos.x - bufferZone < rectStart.x && rectStart.x < slice.pos.x + slice.size.x + bufferZone &&
-				texture->size.y - slice.pos.y - slice.size.y - bufferZone < rectStart.y && rectStart.y < texture->size.y - slice.pos.y + bufferZone)
-			{
-				if (multiSelected)
-				{
-					bool add = false;
-
-					if (prevSelSlice != -1)
-					{
-						selectedSlices.push_back(prevSelSlice);
-
-						selSlice = -1;
-					}
-
-					bool alreadyAdded = false;
-
-					for (int j = 0; j < selectedSlices.size(); j++)
-					{
-						if (selectedSlices[j] == i)
-						{
-							alreadyAdded = true;
-
-							selectedSlices.erase(selectedSlices.begin() + j);
-
-							break;
-						}
-					}
-
-					if (!alreadyAdded)
-					{
-						selectedSlices.push_back(i);
-
-						selSlice = -1;
-
-						break;
-					}
-
-					break;
-				}
-				else
-				{
-					selSlice = i;
-
-					FillRects();
-				}
-			}
-		}
-
-		if (!multiSelected && selSlice == -1)
-		{
-			selectedSlices.clear();
-		}
-
-		if (selSlice != -1)
-		{
-			if (selectedSlices.size() > 0)
-			{
-				for (int j = 0; j < selectedSlices.size(); j++)
-				{
-					if (selectedSlices[j] == selSlice)
-					{
-						selSlice = -1;
-						break;
-					}
-				}
-
-				if (selSlice == -1)
-				{
-					drag = Drag::DragRects;
-				}
-				else
-				{
-					selectedSlices.clear();
-					drag = Drag::DragSelectRect;
-				}
-			}
-			else
-			if (prevSelSlice != selSlice)
-			{
-				drag = Drag::DragSelectRect;
-			}
-			else
-			{
-				drag = Drag::DragRects;
-
-				selCol = -1;
-				selRow = -1;
-
-				for (int i = 0; i < rectHeight; i++)
-					for (int j = 0; j < rectWidth; j++)
-					{
-						Math::Vector2 point = points[rectWidth * i + j];
-
-						if (point.x - 7 < rectStart.x && rectStart.x < point.x + 7 &&
-							point.y - 7 < rectStart.y && rectStart.y < point.y + 7)
-						{
-							selRow = i;
-							selCol = j;
-						}
-					}
-			}
-		}*/
 	}
 
 	void TileSetWindow::OnLeftMouseUp()
 	{
-		/*if (drag == Drag::DragNewSlice)
-		{
-			AssetTexture::Slice slice;
-			slice.pos = Math::Vector2(fminf(rectStart.x, rectEnd.x), texture->size.y - fmaxf(rectStart.y, rectEnd.y));
-			slice.size = Math::Vector2(fmaxf(rectStart.x, rectEnd.x), fmaxf(rectStart.y, rectEnd.y)) - Math::Vector2(fminf(rectStart.x, rectEnd.x), fminf(rectStart.y, rectEnd.y));
-
-			slice.name = StringUtils::PrintTemp("Slice%i", texture->slices.size());
-
-			selSlice = (int)texture->slices.size();
-			texture->slices.push_back(slice);
-
-			FillRects();
-		}
-		else
-		if (drag == Drag::DragRects)
-		{
-			if (selSlice == -1)
-			{
-				for (int j = 0; j < selectedSlices.size(); j++)
-				{
-					auto& slice = texture->slices[selectedSlices[j]];
-					slice.pos.x = floorf(slice.pos.x);
-					slice.pos.y = floorf(slice.pos.y);
-				}
-			}
-			else
-			{
-				FillRects();
-			}
-		}
-
-
-		selCol = -1;
-		selRow = -1;
 		drag = Drag::DragNone;
-		inDragAndDrop = false;*/
 	}
 }
 
--- ENgine/Editor/TileSetWindow.h
+++ ENgine/Editor/TileSetWindow.h
@@ -9,7 +9,25 @@ namespace Oak
 {
 	class TileSetWindow
 	{
+		enum class Drag
+		{
+			DragNone,
+			DragField
+		};
+
+		Drag drag = Drag::DragNone;
 		bool needSetSize = true;
+
+		Math::Vector2 prevMs;
+
+		Math::Vector2 lastViewportSize = Math::Vector2(800.f, 600.0f);
+
+		Math::Vector2 camPos;
+		float camZoom = 1.0f;
+
+		bool viewportCaptured = false;
+		bool vireportHowered = false;
+		bool imageFocused = false;
 
 	public:
 
@@ -24,6 +42,9 @@ namespace Oak
 
 		void ImGui();
 		void DrawViewport(Math::Vector2 viewportSize);
+
+		void ShowToolbar();
+		void ShowImage();
 
 		void OnMouseMove(Math::Vector2 ms, bool drag);
 
