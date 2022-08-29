980d322456669601401cbbec940c8516a443593c
ESoul <enginee777@gmail.com>
2022-08-28 19:38:02 -0700

    Fixing warnings

--- Bin/ENgine/CppBuild/src/Gameplay.h
+++ Bin/ENgine/CppBuild/src/Gameplay.h
@@ -2,6 +2,6 @@
 
 #include "Root/Scenes/SceneEntity.h"
 
-extern "C" CLASS_DECLSPEC void register_code(eastl::vector<Oak::ClassFactorySceneEntity*>& Decls);
-extern "C" CLASS_DECLSPEC void recreate_entites(eastl::vector<Oak::SceneEntity*>& entities);
-extern "C" CLASS_DECLSPEC void unregister_code(eastl::vector<Oak::ClassFactorySceneEntity*>& Decls);
+extern "C" __declspec(dllexport) void register_code(eastl::vector<Oak::ClassFactorySceneEntity*>& Decls);
+extern "C" __declspec(dllexport) void recreate_entites(eastl::vector<Oak::SceneEntity*>& entities);
+extern "C" __declspec(dllexport) void unregister_code(eastl::vector<Oak::ClassFactorySceneEntity*>& Decls);
