6655d71090bc06e331d65c3b3ce81c582345476a
ESoul <enginee777@gmail.com>
2022-08-31 08:41:09 -0700

    Updating files after reorganization
    Change-Id: I6e48659190430aeb5a24587862d077fae3b9717a

--- Editor/CopyCppBuild.bat
+++ Editor/CopyCppBuild.bat
@@ -1,7 +1,7 @@
 RMDIR /s /q ENgine\CppBuild\include
-xcopy ..\Libs\vjson\*.h ENgine\CppBuild\include\Libs\vjson\ /sy
-xcopy ..\Libs\eastl\include\*.* ENgine\CppBuild\include\Libs\eastl\include\ /sy
-xcopy ..\Libs\fmod\include\*.* ENgine\CppBuild\include\Libs\fmod\include\ /sy
-xcopy ..\Libs\physx\include\*.* ENgine\CppBuild\include\Libs\physx\include\ /sy
-xcopy ..\ENgine\*.h ENgine\CppBuild\include\ENgine\ /sy
+xcopy ..\ENgine\Thirdparty\vjson\*.h ENgine\CppBuild\include\Libs\vjson\ /sy
+xcopy ..\ENgine\Thirdparty\eastl\include\*.* ENgine\CppBuild\include\Libs\eastl\include\ /sy
+xcopy ..\ENgine\Thirdparty\fmod\include\*.* ENgine\CppBuild\include\Libs\fmod\include\ /sy
+xcopy ..\ENgine\Thirdparty\physx\include\*.* ENgine\CppBuild\include\Libs\physx\include\ /sy
+xcopy ..\ENgine\Source\*.h ENgine\CppBuild\include\ENgine\ /sy
 copy Oak.dll ENgine\ExportBinaries\Oak.dll
--- Editor/PrepareBuild.bat
+++ Editor/PrepareBuild.bat
@@ -1,49 +1,54 @@
 call CopyCppBuild.bat
-cd ../Doxygen
+cd ../ENgine/Doxygen
 call gen_doc.bat
-cd ../
-F:\VS\VS2019\Common7\IDE\devenv.exe /Build Release Projects\Windows\Oak.sln /Project Runner
-RMDIR /s /q ENgineRelease\
-xcopy Bin\*.* ENgineRelease\ /sy
+cd ../../
+F:\VS\VS2019\Common7\IDE\devenv.exe /Build Release ENgine\Projects\Windows\Oak.sln /Project Runner
+RMDIR /s /q EditorRelease\
+xcopy Editor\*.* EditorRelease\Editor\ /sy
 
-del ENgineRelease\CopyCppBuild.bat
-del ENgineRelease\eastl.iobj
-del ENgineRelease\eastl.ipdb
-del ENgineRelease\eastl_Debug.dll
-del ENgineRelease\eastl_Debug.ilk
-del ENgineRelease\imgui.ini
-del ENgineRelease\Oak.iobj
-del ENgineRelease\Oak.ipdb
-del ENgineRelease\Oak_Debug.dll
-del ENgineRelease\Oak_Debug.ilk
-del ENgineRelease\OakEditor.iobj
-del ENgineRelease\OakEditor.ipdb
-del ENgineRelease\OakEditor_Debug.exe
-del ENgineRelease\OakEditor_Debug.ilk
-del ENgineRelease\PhysX_64_debug.dll
-del ENgineRelease\PhysXCommon_64_debug.dll
-del ENgineRelease\PhysXCooking_64_debug.dll
-del ENgineRelease\PhysXFoundation_64_debug.dll
-del ENgineRelease\PrepareBuild.bat
-del ENgineRelease\editor
+del EditorRelease\Editor\CopyCppBuild.bat
+del EditorRelease\Editor\eastl.iobj
+del EditorRelease\Editor\eastl.ipdb
+del EditorRelease\Editor\eastl_Debug.dll
+del EditorRelease\Editor\eastl_Debug.ilk
+del EditorRelease\Editor\imgui.ini
+del EditorRelease\Editor\Oak.iobj
+del EditorRelease\Editor\Oak.ipdb
+del EditorRelease\Editor\Oak_Debug.dll
+del EditorRelease\Editor\Oak_Debug.ilk
+del EditorRelease\Editor\OakEditor.iobj
+del EditorRelease\Editor\OakEditor.ipdb
+del EditorRelease\Editor\OakEditor_Debug.exe
+del EditorRelease\Editor\OakEditor_Debug.ilk
+del EditorRelease\Editor\PhysX_64_debug.dll
+del EditorRelease\Editor\PhysXCommon_64_debug.dll
+del EditorRelease\Editor\PhysXCooking_64_debug.dll
+del EditorRelease\Editor\PhysXFoundation_64_debug.dll
+del EditorRelease\Editor\PrepareBuild.bat
 
-RMDIR /s /q ENgineRelease\Logs
+RMDIR /s /q EditorRelease\Editor\Logs
 
-del ENgineRelease\DebugInfo\eastl_Debug.pdb
-del ENgineRelease\DebugInfo\Oak_Debug.pdb
-del ENgineRelease\DebugInfo\OakEditor_Debug.pdb
+del EditorRelease\Editor\DebugInfo\eastl_Debug.pdb
+del EditorRelease\Editor\DebugInfo\Oak_Debug.pdb
+del EditorRelease\Editor\DebugInfo\OakEditor_Debug.pdb
 
-del ENgineRelease\ENgine\ExportBinaries\Oak_Debug.exe
-del ENgineRelease\ENgine\ExportBinaries\Oak_Debug.ilk
+del EditorRelease\Editor\ENgine\ExportBinaries\Oak_Debug.exe
+del EditorRelease\Editor\ENgine\ExportBinaries\Oak_Debug.ilk
 
-RMDIR /s /q ENgineRelease\DemoProjects\Beatemup\_Code
-del ENgineRelease\DemoProjects\Beatemup\gameplay_Debug.dll
+del EditorRelease\Editor\projects
 
-RMDIR /s /q ENgineRelease\DemoProjects\Tanks\_Code
-del ENgineRelease\DemoProjects\Tanks\gameplay_Debug.dll
+xcopy Samples\*.* EditorRelease\Samples\ /sy
 
-del ENgineRelease\projects
-copy ENgineRelease\ENgine\defaultProjects ENgineRelease\projects
-del ENgineRelease\ENgine\defaultProjects
+RMDIR /s /q EditorRelease\Samples\Beatemup\VsProj
+RMDIR /s /q EditorRelease\Samples\Beatemup\Binaries
+
+RMDIR /s /q EditorRelease\Samples\Tanks\VsProj
+RMDIR /s /q EditorRelease\Samples\Tanks\Binaries
+
+RMDIR /s /q EditorRelease\Samples\SimplePlatformer\VsProj
+RMDIR /s /q EditorRelease\Samples\SimplePlatformer\Binaries
+
+copy EditorRelease\Samples\projects EditorRelease\Editor\projects
+del EditorRelease\Samples\projects
 
 pause
