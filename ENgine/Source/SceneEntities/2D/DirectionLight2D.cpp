3e96e1ac481d18c4cb83bdc10b382c80b8c2c763
ESoul <enginee777@gmail.com>
2023-08-27 21:06:12 -0700

    Missing file

---
+++ ENgine/Source/SceneEntities/2D/DirectionLight2D.cpp
@@ -0,0 +1,64 @@
+
+#include "DirectionLight2D.h"
+#include "Root/Root.h"
+#include "Editor/EditorDrawer.h"
+
+namespace Orin
+{
+	ENTITYREG(SceneEntity, DirectionLight2D, "2D/Lights", "DirectionLight2D")
+
+	META_DATA_DESC(DirectionLight2D)
+		BASE_SCENE_ENTITY_PROP(DirectionLight2D)
+		COLOR_PROP(DirectionLight2D, color, COLOR_WHITE, "Visual", "Color")
+		FLOAT_PROP(DirectionLight2D, intesity, 1.0f, "Visual", "intesity", "intesity")
+		BOOL_PROP(DirectionLight2D, isSecondaryLight, false, "Visual", "isSecondaryLight", "isSecondaryLight")
+	META_DATA_DESC_END()
+
+	DirectionLight2D::DirectionLight2D() : SceneEntity()
+	{
+		transform.size.x = 100.0f;
+	}
+
+	void DirectionLight2D::Init()
+	{
+		transform.objectType = ObjectType::Object2D;
+		transform.transformFlag = TransformFlag::MoveXYZ | TransformFlag::RectSizeX | TransformFlag::RotateZ;
+
+		Tasks(true)->AddTask(199, this, (Object::Delegate) &DirectionLight2D::Draw);
+
+		GetScene()->AddToGroup(this, "DirectionLight2D");
+	}
+
+	void DirectionLight2D::Draw(float dt)
+	{
+		if (GetScene()->IsPlaying())
+		{
+			return;
+		}
+
+		transform.size.x = 100.0f;
+		transform.size.y = transform.size.x;
+
+		if (IsVisible())
+		{
+			auto pos = Sprite::ToPixels(transform.GetGlobal().Pos());
+			float scale = 640.0f / Sprite::GetPixelsHeight();
+
+			auto size = Math::Vector2((float)editorDrawer.directionalTex->GetWidth(), (float)editorDrawer.directionalTex->GetHeight()) / scale;
+			
+			editorDrawer.DrawSprite(editorDrawer.directionalTex, Math::Vector2(pos.x, pos.y), size, -Math::Vector2(size.x, -size.y) * 0.5f, Math::Radian * transform.rotation.z, COLOR_WHITE);
+		}
+	}
+
+	void DirectionLight2D::GetBBox(Math::Vector3& vMin, Math::Vector3& vMax)
+	{
+		auto mat = transform.GetGlobal();
+
+		float scale = 640.0f / Sprite::GetPixelsHeight();
+
+		auto size = Sprite::ToUnits(Math::Vector2((float)editorDrawer.directionalTex->GetWidth(), (float)editorDrawer.directionalTex->GetHeight()) / scale);
+
+		vMin = transform.GetGlobal().Pos() - size * 0.5f;
+		vMax = transform.GetGlobal().Pos() + size * 0.5f;
+	}
+}
---
+++ ENgine/Source/SceneEntities/2D/DirectionLight2D.h
@@ -0,0 +1,32 @@
+
+#pragma once
+
+#include "Root/Scenes/SceneEntity.h"
+#include "Support/MetaData.h"
+#include "Support/Sprite.h"
+#include "root/Assets/AssetTexture.h"
+
+namespace Orin
+{
+	class DirectionLight2D : public SceneEntity
+	{
+	public:
+
+		META_DATA_DECL_BASE(DirectionLight2D)
+
+		Color color;
+		float intesity = 1.0f;
+		bool isSecondaryLight = false;
+
+	#ifndef DOXYGEN_SKIP
+
+		DirectionLight2D();
+		virtual ~DirectionLight2D() = default;
+
+		void Init() override;
+		void Draw(float dt);
+
+		void GetBBox(Math::Vector3& vMin, Math::Vector3& vMax) override;
+	#endif
+	};
+}
