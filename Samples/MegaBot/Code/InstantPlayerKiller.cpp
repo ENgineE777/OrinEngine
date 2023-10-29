6974872aa407169cfe125d04c24968bd1b7d3295
ESoul <enginee777@gmail.com>
2023-10-29 09:39:06 -0700

    Less parameters

--- Samples/MegaBot/Code/InstantPlayerKiller.cpp
+++ Samples/MegaBot/Code/InstantPlayerKiller.cpp
@@ -9,14 +9,15 @@ namespace Orin
 
 	META_DATA_DESC(InstantPlayerKiller)
 		BASE_SCENE_ENTITY_PROP(InstantPlayerKiller)
-		COLOR_PROP(InstantPlayerKiller, color, COLOR_YELLOW, "Geometry", "color")
 		INT_PROP(InstantPlayerKiller, physGroup, 1, "Physics", "Physical group", "Physical group")
-		BOOL_PROP(InstantPlayerKiller, visibleDuringPlay, true, "Physics", "visibleDuringPlay", "Show collision during play")
 	META_DATA_DESC_END()
 
 	void InstantPlayerKiller::Init()
 	{
 		PhysTriger2D::Init();
+
+		visibleDuringPlay = false;
+		color = COLOR_YELLOW;
 
 		if (prefabInstance)
 		{
